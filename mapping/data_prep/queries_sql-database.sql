27e903f6c50aee00f29b587e38a3c06751f24995
nevrome <clemens@nevrome.de>
2016-01-12 15:57:16 +0100

    Modification of replacement-values and drafts for some of the queries in radiocarbon3_prep.R

--- mapping/data_prep/queries_sql-database.sql
+++ mapping/data_prep/queries_sql-database.sql
@@ -4,3 +4,42 @@ select LABNR, MATERIAL, SPECIES, SITE, PERIOD, CULTURE, LATITUDE, LONGITUDE, MET
 from dates 
 where SITE = 'Karanovo';
 
+/*youngest dates of each site*/
+select a.SITE, a.LABNR, a.CALAGE
+from dates a
+  left join dates b
+    on a.SITE = b.SITE and a.CALAGE > b.CALAGE
+where b.CALAGE is NULL;
+
+/*oldest dates of each site*/
+select a.SITE, a.LABNR, a.CALAGE
+from dates a
+  left join dates b
+    on a.SITE = b.SITE and a.CALAGE < b.CALAGE
+where b.CALAGE is NULL; 
+
+/*Empty table for date-selection*/
+create table dates_wo select * from dates where 1=0;
+
+/*exclude oldest dates of each site*/
+/*insert into dates_wo*/
+select *
+from dates c
+where c.LABNR <> all(select a.LABNR
+                 from dates a
+                 left join dates b
+                 on a.SITE = b.SITE and a.CALAGE < b.CALAGE
+                 where b.CALAGE is NULL)
+
+/*exclude oldest and second oldets dates of each site
+-> needs dates_wo*/
+select *
+from dates_wo c
+where c.LABNR <> all(select a.LABNR
+                 from dates_wo a
+                 left join dates_wo b
+                 on a.SITE = b.SITE and a.CALAGE < b.CALAGE
+                 where b.CALAGE is NULL);
+
+                 
+  
