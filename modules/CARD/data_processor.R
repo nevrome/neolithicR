ccdca06a0771a6c2ee35c1a543125a16be107b8b
nevrome <clemens@nevrome.de>
2016-04-21 17:14:51 +0200

    First draft of a module to connect to CARD. (https://github.com/dirkseidensticker/CARD)

---
+++ modules/CARD/data_processor.R
@@ -0,0 +1,44 @@
+# load libraries
+library(RCurl)
+library(RSQLite)
+
+# read data from URL
+myfile <- getURL(
+  'https://raw.githubusercontent.com/dirkseidensticker/CARD/master/data/CARD.csv', 
+  ssl.verifyhost = FALSE, 
+  ssl.verifypeer = FALSE
+)
+
+CARD <- read.csv(
+  textConnection(myfile), 
+  header = T, 
+  sep = ","
+)
+
+# adjust attribute names
+colnames(CARD)[c(10,11,12)] <- c("LATITUDE", "LONGITUDE", "REFERENCE")
+
+# add key attributes ORIGIN and ID
+CARD <- data.frame(
+  ORIGIN = rep("CARD", nrow(CARD)),
+  ID = 1:nrow(CARD), 
+  CARD
+)
+
+# connect to database and load the content of the table "dates" into a dataframe
+con <- dbConnect(RSQLite::SQLite(), "data/rc.db")
+datestable = dbGetQuery(con, 'select * from dates')
+
+# merge database with new data
+CARDres <- merge(
+  CARD, 
+  datestable, 
+  all.x = TRUE, all.y = TRUE, 
+  incomparables = NULL
+)
+
+# write results into database
+dbWriteTable(con, "dates", CARDres, append = TRUE)
+
+# test new state
+# test <- dbGetQuery(con, 'select * from dates')
