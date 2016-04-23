8e95b50abd82fbb51ca27ff238dee89295d3465a
nevrome <clemens@nevrome.de>
2016-04-24 00:29:23 +0200

    Radical simplification of the shiny app.

--- modules/db_to_rdata/db_to_rdata.R
+++ modules/db_to_rdata/db_to_rdata.R
@@ -5,6 +5,10 @@ library(RSQLite)
 con <- dbConnect(RSQLite::SQLite(), "data/rc.db")
 datestable = dbGetQuery(con, 'select * from dates')
 
+# remove dates without long and lat value
+datestable <- datestable[!is.na(datestable$LONGITUDE),]
+datestable  <- datestable[!is.na(datestable$LATITUDE),]
+
 # store data.frame datestable as a .RData object into the app file system
 save(datestable, file = "modules/radiocarbon3/data/c14data.RData")
 
