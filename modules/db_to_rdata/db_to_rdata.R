377290bf157537e1056bed0c2d16c2e6675e0a91
nevrome <clemens@nevrome.de>
2016-04-23 21:10:04 +0200

    Adding a small module for the transformation from sqlite db to .RData object (performance)

---
+++ modules/db_to_rdata/db_to_rdata.R
@@ -0,0 +1,12 @@
+# load libraries
+library(RSQLite)
+
+# connect to database and load the content of the table "dates" into a dataframe datestable
+con <- dbConnect(RSQLite::SQLite(), "data/rc.db")
+datestable = dbGetQuery(con, 'select * from dates')
+
+# store data.frame datestable as a .RData object into the app file system
+save(datestable, file = "modules/radiocarbon3/data/c14data.RData")
+
+# test
+# load(file = "modules/radiocarbon3/data/c14data.RData")
