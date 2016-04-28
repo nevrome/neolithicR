3e810277b14514f2a4d3ca09ac323836e2c80b1c
nevrome <clemens@nevrome.de>
2016-04-29 00:17:30 +0200

    Added further data exclusion to db-.rdata module

--- modules/db_to_rdata/db_to_rdata.R
+++ modules/db_to_rdata/db_to_rdata.R
@@ -1,5 +1,6 @@
 # load libraries
 library(RSQLite)
+library(dplyr)
 
 # connect to database and load the content of the table "dates" into a dataframe datestable
 con <- dbConnect(RSQLite::SQLite(), "data/rc.db")
@@ -8,6 +9,21 @@ datestable = dbGetQuery(con, 'select * from dates')
 # remove dates without long and lat value
 datestable <- datestable[!is.na(datestable$LONGITUDE),]
 datestable  <- datestable[!is.na(datestable$LATITUDE),]
+
+# transform long and lat value to numeric
+datestable$LONGITUDE <- as.numeric(datestable$LONGITUDE)
+datestable$LATITUDE <- as.numeric(datestable$LATITUDE)
+
+# remove wrong values
+datestable <- filter(datestable,
+       datestable$LONGITUDE < 180 &
+       datestable$LONGITUDE > -180 &
+       datestable$LATITUDE < 90 &
+       datestable$LATITUDE > -90
+       )
+
+datestable <- datestable[!(datestable$LONGITUDE > 180 || datestable$LONGITUDE < -180),]
+datestable <- datestable[!(datestable$LATITUDE > 90 || datestable$LATITUDE < -90),]
 
 # sort by CALAGE
 datestable <- datestable[order(datestable$CALAGE, decreasing=TRUE),]
