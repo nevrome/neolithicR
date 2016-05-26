999794c5d4d5388a7e57d82fd32cd7496cd0d4e5
nevrome <clemens@nevrome.de>
2016-05-26 16:00:18 +0200

    Renaming CALPAL module and beginning to work on RADON import

---
+++ modules/RADON/data_processor.R
@@ -0,0 +1,49 @@
+# load libraries
+library(RCurl)
+library(RSQLite)
+
+# read data from URL
+myfile <- getURL(
+  '134.245.38.100/radondownload/radondaily.txt', 
+  ssl.verifyhost = FALSE, 
+  ssl.verifypeer = FALSE,
+  .encoding = "UTF-8"
+)
+
+RADON <- read.table(
+  textConnection(myfile), 
+  header = T, 
+  sep = "\t",
+  strip.white = TRUE,
+  fill = TRUE,
+  quote = "",
+  stringsAsFactors = FALSE
+)
+
+# # remove ID column and empty colums at the end
+# CALPAL <- CALPAL[,2:20]
+# 
+# # add key attributes ORIGIN and ID
+# CALPAL <- data.frame(
+#   ORIGIN = rep("CALPAL", nrow(CALPAL)),
+#   ID = 1:nrow(CALPAL), 
+#   CALPAL
+# )
+# 
+# # connect to database and load the content of the table "dates" into a dataframe
+# con <- dbConnect(RSQLite::SQLite(), "data/rc.db")
+# datestable = dbGetQuery(con, 'select * from dates')
+# 
+# # merge database with new data
+# CALPALres <- merge(
+#   CALPAL, 
+#   datestable, 
+#   all.x = TRUE, all.y = TRUE, 
+#   incomparables = NULL
+# )
+# 
+# # write results into database
+# dbWriteTable(con, "dates", CALPALres, overwrite = TRUE)
+# 
+# # test new state
+# # test <- dbGetQuery(con, 'select * from dates')
