0e0d8d6df458130fb63ae546e79853629259d73d
nevrome <clemens@nevrome.de>
2017-02-04 00:38:26 +0100

    improved code for date calibration (thanks to Martin Hinz)

--- modules/calibration/bchron_cal.R
+++ modules/calibration/bchron_cal.R
@@ -10,13 +10,11 @@ pb <- txtProgressBar(
 library(Bchron)
 library(RSQLite)
 library(magrittr)
+library(plyr)
 
 # connect to database and load the content of the table "dates" into a dataframe
 con <- dbConnect(RSQLite::SQLite(), "data/rc.db")
 datestable = dbGetQuery(con, 'select * from dates')
-
-# precalculated values
-afaktor <- (1-0.683)/2
 
 # determine dates that are out of the range of the calcurve can not be calibrated
 which(datestable$C14AGE < 100) -> toosmall
@@ -33,30 +31,25 @@ datestable$CALSTD[outofrange] <- datestable$C14STD[outofrange]
 setTxtProgressBar(pb, 5)
 
 
+# precalculated values
+threshold <- (1-0.9545)/2
 
 # calibration
-interval683 <- datestable[-outofrange, ] %>%
+interval95 <- datestable[-outofrange, ] %$% 
   # calculate density vector
   BchronCalibrate(
-    ages = use_series(., C14AGE),
-    ageSds = use_series(., C14STD),
-    calCurves = rep("intcal13", nrow(.))
-  ) %>%
-  set_names(
-    sprintf("date%i", 1:length(.))
-  ) %>%
-  # draw 1000 samples from density vector
-  # https://github.com/andrewcparnell/Bchron/blob/master/vignettes/Bchron.Rmd
-  sapply(
-    ., function(x){sample(x$ageGrid, size = 1000, replace = TRUE, prob = x$densities)}
+    ages      = C14AGE,
+    ageSds    = C14STD,
+    calCurves = rep("intcal13", nrow(.)),
+    eps       = 1e-06
   ) %>% 
-  # determine upper and lower border of 68% interval of density distribution
-  #interval95 <- apply(age_samples, 2, quantile, prob = c(0.025, 0.975))
-  apply(
-    ., 2, quantile, prob = c(afaktor, 1-afaktor)
-  ) %>%
-  t %>%
-  as.data.frame
+  plyr::ldply(., function(x) {
+      x$densities            %>% cumsum -> a
+      which(a <= threshold)  %>% max -> my_min
+      which(a > 1-threshold) %>% min -> my_max
+      x$ageGrid[c(my_min, my_max)]
+    }
+  ) 
 
 
 
@@ -66,9 +59,11 @@ setTxtProgressBar(pb, 95)
 
  
 # preliminary: take the mean of the borders as CALAGE and the distance
-# of CALAGE to the upper and lower 68.3% interval as CALSTD 
-top <- round(interval683[,2])
-amean <- apply(interval683, 1, function(x){round(mean(x))})
+# of CALAGE to the upper and lower 95.45% interval as CALSTD 
+top <- round(interval95[, 3])
+amean <- apply(interval95[, 2:3], 1, function(x){round(mean(x))})
+
+# write result back into datestable
 datestable$CALAGE[-outofrange] <- amean
 datestable$CALSTD[-outofrange] <- top - amean
 
