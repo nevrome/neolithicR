86d2147d42a4e5440475ef04c71a15ffb05c0655
nevrome <clemens@nevrome.de>
2017-11-13 23:59:23 +0100

    first draft of a data prep script

---
+++ query_and_prep.R
@@ -0,0 +1,16 @@
+library(magrittr)
+
+datestable <- c14bazAAR::get_all_dates() %>%
+  dplyr::sample_n(1000) %>%
+  c14bazAAR::as.c14_date_list() %>%
+  c14bazAAR::clean() %>%
+  c14bazAAR::calibrate() %>%
+  c14bazAAR::estimate_spatial_quality() %>%
+  c14bazAAR::rm_doubles(mark = TRUE) %>%
+  c14bazAAR::thesaurify() %>%
+  dplyr::arrange(dplyr::desc(calage)) %>%
+  dplyr::mutate(
+    maincolor = rainbow(nrow(.), alpha = NULL, start = 0, end = 2/6)
+  )
+
+save(datestable, file = "neolithicRC/data/c14data.RData")
